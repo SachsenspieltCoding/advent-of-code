30ffb59a18d0345562b6fed3ee9312a82a567b12
SachsenspieltCoding <coding@sachsenspielt.de>
2025-12-01 13:28:19 +0100

    clean up repo

--- src/main/java/de/sachsenspielt/adventofcode/Main.java
+++
@@ -1,9 +0,0 @@
-package de.sachsenspielt.adventofcode;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("=== AOC 2024 by SachsenspieltCoding ===");
-        System.out.println();
-        new Day01().run();
-    }
-}
